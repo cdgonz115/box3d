1ff3e311b588cc8e2bd27be87d20122703f02cab
cdgonz115 <christiandgonzalez115@gmail.com>
2026-09-22 14:20:30 -0700

    feat: Sample Gravity Fixes
    Summary:
    -Made fixes to the gravity sample to make them work both in single and double mode
    -Added build double to the .gitignore

--- samples/sample_gravity.cpp
+++ samples/sample_gravity.cpp
@@ -96,7 +96,8 @@ public:
             b3RayResult result = b3World_CastRayClosest(m_worldId, pickRay.origin, pickRay.translation, b3DefaultQueryFilter());
             for(int i = 0; i < m_markers.size(); i++)
             {
-                if(b3Distance(m_markers[i].position, result.point) < PICK_RADIUS)
+                b3Vec3 delta = b3SubPos(m_markers[i].position,result.point);
+                if(b3Length(delta) < PICK_RADIUS)
                 {
                     b3Body_RemoveGravitySourceAt(m_sphereBodyId, i);
                     m_markers.erase(m_markers.begin()+i);
@@ -123,8 +124,10 @@ public:
     {
         for(const SampleSceneGravityMarker& marker: m_markers)
         {
-            b3Sphere sphere = { marker.position, 0.2f};
-            DrawSolidSphere(b3Transform_identity, sphere, MakeColor(b3_colorGreen));
+            b3WorldTransform transform =b3WorldTransform_identity;
+            transform.p = marker.position;
+            b3Sphere sphere = {b3Vec3_zero, 0.2f};
+            DrawSolidSphere(transform,sphere, MakeColor(b3_colorGreen));
         }
 
         Sample::Step();
@@ -179,7 +182,7 @@ public:
         
         float step = 360 / numberOfBalls;
         
-        b3Vec3 center = { 0.0f, 4.0f };
+        b3Pos center = { 0.0f, 4.0f , 0.0f};
         
         for (int i = 0 ; i < numberOfBalls; i++)
         {
@@ -274,7 +277,7 @@ public:
         
         float step = 360 / numberOfBalls;
         
-        b3Vec3 center = { 0.0f, 4.0f };
+        b3Pos center = { 0.0f, 4.0f , 0.0f};
         
         for (int i = 0 ; i < numberOfBalls; i++)
         {
